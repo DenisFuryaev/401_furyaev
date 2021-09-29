5437bd07f464d2c743423e7750431a21126d80d9
DenisFuryaev <denis.furyaev@gmail.com>
2021-09-29 16:03:55 +0300

    object detection working for files in directory

--- ObjectDetectionConsoleApp/Program.cs
+++ ObjectDetectionConsoleApp/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using YOLOv4MLNet.DataStructures;
+using static Microsoft.ML.Transforms.Image.ImageResizingEstimator;
 
-namespace ObjectDetectionConsoleApp
+namespace YOLOv4MLNet
 {
     class Program
     {
-        static void Main(string[] args)
+        const string imageFolder = @"..\..\..\..\ObjectDetectionLib\assets\images";
+        static List<YoloV4Result> modelOutput;
+        static Dictionary<string, int> modelDictOutput = new Dictionary<string, int>();
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var sw = new Stopwatch();
+            sw.Start();
+
+            modelOutput = Predictor.MakePredictios(imageFolder);
+            Console.WriteLine();
+
+            foreach (YoloV4Result entry in modelOutput)           
+                if (modelDictOutput.ContainsKey(entry.Label))
+                    modelDictOutput[entry.Label] += 1;
+                else
+                    modelDictOutput.Add(entry.Label, 1);
+
+            foreach (KeyValuePair<string, int> entry in modelDictOutput)
+            {
+                Console.WriteLine($"    {entry.Value} {entry.Key}(s)");
+            }
+            
+
+            sw.Stop();
+            Console.WriteLine($"\nDone in {sw.ElapsedMilliseconds}ms.");
         }
     }
 }
--- ObjectDetectionLib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ObjectDetectionLib
-{
-    public class Class1
-    {
-    }
-}
