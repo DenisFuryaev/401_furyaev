284dffb86aab0211680592ca9af03948fb0e8caa
DenisFuryaev <denis.furyaev@gmail.com>
2021-12-01 13:59:30 +0300

    first commit for task4

--- (4 task) Client and Server/ServerAndClient/App.xaml.cs
+++ (4 task) Client and Server/ServerAndClient/App.xaml.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Data;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ObjectDetectionUI
+namespace ServerAndClient
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- (4 task) Client and Server/ServerAndClient/MainWindow.xaml.cs
+++ (4 task) Client and Server/ServerAndClient/MainWindow.xaml.cs
@@ -12,9 +12,37 @@ using System.Collections.Specialized;
 using System.Collections;
 using System.Windows.Controls;
 using System.Windows.Media;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Drawing;
+
 
 namespace ObjectDetectionUI
 {
+    class ImageObject
+    {
+        public int ImageObjectId { get; set; }
+        public string Label { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public byte[] ObjectImage { get; set; }
+    }
+    class ObjectContext : DbContext
+    {
+        public DbSet<ImageObject> ImageObjects { get; set; }
+
+        public ObjectContext() : base()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder o) => o.UseSqlite("Data Source=database.db");
+    }
+
+
+
     public class ObjectsData : IEnumerable<string>, INotifyCollectionChanged
     {
         public class Value
@@ -58,12 +86,12 @@ namespace ObjectDetectionUI
                     else
                     {
                         DetectedObjects[obj.Label].Count += 1;
-                        DetectedObjects[obj.Label].Dict.Add(fileName, new List<float[]>{ obj.BBox });
+                        DetectedObjects[obj.Label].Dict.Add(fileName, new List<float[]> { obj.BBox });
                     }
 
                 else // outer dictionary doesn't have this object
                 {
-                    DetectedObjects.Add(obj.Label, new Value(1, new Dictionary<string, List<float[]>> { { fileName, new List<float[]>{ obj.BBox } } }));
+                    DetectedObjects.Add(obj.Label, new Value(1, new Dictionary<string, List<float[]>> { { fileName, new List<float[]> { obj.BBox } } }));
                 }
 
             CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
@@ -71,7 +99,7 @@ namespace ObjectDetectionUI
 
         public IEnumerator<string> GetEnumerator()
         {
-            foreach(string obj in DetectedObjects.Keys)
+            foreach (string obj in DetectedObjects.Keys)
             {
                 yield return obj;
             }
@@ -101,13 +129,94 @@ namespace ObjectDetectionUI
 
             ProcessedFilesListBox.ItemsSource = ProcessedFiles;
             ObjectsListBox.ItemsSource = objectsDict;
+
+            ConfigureDatabase();
         }
         private void PredictorEventHandler(string filePath, List<YoloV4Result> objectsList)
         {
             lock (myLocker)
             {
-                Dispatcher.Invoke(new Action(() => { objectsDict.Add(objectsList, filePath); ProcessedFiles.Add(Path.GetFileName(filePath)); }));
+                Dispatcher.Invoke(new Action(() => { 
+                    objectsDict.Add(objectsList, filePath);
+                    ProcessedFiles.Add(Path.GetFileName(filePath));
+                    // putting objects in database
+                    using (ObjectContext context = new ObjectContext())
+                    {
+                        foreach (YoloV4Result detectedObject in objectsList)
+                        {
+
+                            int x = (int)detectedObject.BBox[0];
+                            int y = (int)detectedObject.BBox[1];
+                            int width = (int)detectedObject.BBox[2] - x;
+                            int height = (int)detectedObject.BBox[3] - y;
+
+                            System.Drawing.Image image = System.Drawing.Image.FromFile(filePath);
+                            Bitmap bmpImage = new Bitmap(image);
+                            Bitmap croppedImage = bmpImage.Clone(new Rectangle(x, y, width, height), bmpImage.PixelFormat);
+                            byte[] blob = ImageToByteArray(croppedImage);
+
+                            if (DatabaseHasObject(x, y, width, height, blob))
+                                continue;
+
+                            ImageObject imageObject = new ImageObject { Label = detectedObject.Label, X = x, Y = y, Width = width, Height = height, ObjectImage = blob };
+                            context.ImageObjects.Add(imageObject);
+                        }
+                        context.SaveChanges();
+                    }
+                }));
             }
+        }
+        private void ConfigureDatabase()
+        {
+            using (ObjectContext context = new ObjectContext())
+            {
+                // printing database rows to console
+                DatabaseListBox.Items.Clear();
+                var query = context.ImageObjects;
+                var sb = new System.Text.StringBuilder();
+                foreach (var item in query)
+                {
+                    sb = new System.Text.StringBuilder();
+                    sb.Append(String.Format("{0,3} {1,15} {2,5} {3,5} {4,5} {5,5}\n", item.ImageObjectId, item.Label, item.X, item.Y, item.Width, item.Height));
+                    DatabaseListBox.Items.Add(sb.ToString());
+                }
+                
+
+                // deleting all database rows if user selected yes button
+                if (MessageBox.Show("Do you want to delete recorded objects from Database?", "Save file", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    context.ImageObjects.RemoveRange(context.ImageObjects);
+                    context.SaveChanges();
+                }
+            }
+        }
+        private byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            using (var ms = new MemoryStream())
+            {
+                imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+
+                return ms.ToArray();
+            }
+        }
+        private bool DatabaseHasObject(int x, int y, int width, int height, byte[] blob)
+        {
+            bool hasObject = false;
+            using (ObjectContext context = new ObjectContext())
+            {
+                var blobQuery = from item in context.ImageObjects
+                                where item.X == x && item.Y == y && item.Width == width && item.Height == height
+                                select item.ObjectImage;
+                foreach (byte[] blobItem in blobQuery)
+                {
+                    if (blobItem.SequenceEqual(blob))
+                    {
+                        hasObject = true;
+                        break;
+                    }
+                }
+            }
+            return hasObject;
         }
 
         private void ObjectsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -116,6 +225,7 @@ namespace ObjectDetectionUI
                 return;
 
             // draw all images with "selected object"
+            
             ImageListBox.Items.Clear();
             foreach (string filename in objectsDict.DetectedObjects[(string)ObjectsListBox.SelectedItem].Dict.Keys)
             {
@@ -218,4 +328,3 @@ namespace ObjectDetectionUI
         }
     }
 }
-    
